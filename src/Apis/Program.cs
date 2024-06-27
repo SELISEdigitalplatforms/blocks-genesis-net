65e745c94f8afae1b32dd2c8c160cb6a71ed71db
mostafizur044 <mostafiz.mri@gmail.com>
2024-06-27 21:08:42 +0600

    Tracing and logging done

--- src/Apis/Program.cs
+++ src/Apis/Program.cs
@@ -23,8 +23,8 @@ namespace Api1
             ApplicationConfigurations.ConfigureMessage(services, new MessageConfiguration
             {
                 Connection = "Endpoint=sb://blocks-rnd.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=***REMOVED***",
-                Queues = new List<string> { "demo_queue", "demo_queue_1" },
-                Topics = new List<string> { "demo_topic", "demo_topic_1" }
+                Queues = new List<string> { "demo_queue_1" },
+                Topics = new List<string> { "demo_topic_1" }
             });
 
             var app = builder.Build();
