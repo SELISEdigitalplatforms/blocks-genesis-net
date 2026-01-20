964e416faf75e23f2ff99eff4e7972a960fbe6ac
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-01-20 17:51:51 +0600

    fix securityContext generating issue for thirdPartyToken

--- src/Genesis/Configuration/ChangeControllerContext.cs
+++ src/Genesis/Configuration/ChangeControllerContext.cs
@@ -2,7 +2,6 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 using OpenTelemetry;
-using System.Text.Json;
 
 namespace Blocks.Genesis
 {
@@ -10,32 +9,19 @@ namespace Blocks.Genesis
     {
         private readonly ITenants _tenants;
         private readonly IDbContextProvider _dbContextProvider;
-        private readonly IHttpContextAccessor  _httpContextAccessor;
 
         public ChangeControllerContext(ITenants tenants, IDbContextProvider dbContextProvider,
                                        IHttpContextAccessor httpContextAccessor)
         {
             _tenants = tenants;
             _dbContextProvider = dbContextProvider;
-            _httpContextAccessor = httpContextAccessor;
         }
 
         public void ChangeContext(IProjectKey projectKey)
         {
-            string thirdPartyContextHeader = _httpContextAccessor.HttpContext.Request.Headers[BlocksConstants.ThirdPartyContextHeader];
 
-            if (!string.IsNullOrWhiteSpace(thirdPartyContextHeader))
-            {
-                var thirdPartyContext = JsonSerializer.Deserialize<BlocksContext>(thirdPartyContextHeader);
-
-                if (thirdPartyContext != null)
-                {
-                    SetThirdPartyContext(thirdPartyContext);
-                }
-            }
 
             var bc =  BlocksContext.GetContext();
-
             Baggage.SetBaggage("ActualTenantId", bc.TenantId);
 
             if (string.IsNullOrWhiteSpace(projectKey.ProjectKey) || projectKey.ProjectKey == bc?.TenantId) return;
@@ -66,27 +52,6 @@ namespace Blocks.Genesis
 
                 Baggage.SetBaggage("TenantId", projectKey.ProjectKey);
             }
-        }
-
-        private void SetThirdPartyContext(BlocksContext bc)
-        {
-             BlocksContext.SetContext(BlocksContext.Create
-                 (
-                    tenantId: bc.TenantId,
-                    roles: bc?.Roles ?? Enumerable.Empty<string>(),
-                    userId: bc?.UserId ?? string.Empty,
-                    isAuthenticated: bc?.IsAuthenticated ?? false,
-                    requestUri: bc?.RequestUri ?? string.Empty,
-                    organizationId: bc?.OrganizationId ?? string.Empty,
-                    expireOn: bc?.ExpireOn ?? DateTime.UtcNow.AddHours(1),
-                    email: bc?.Email ?? string.Empty,
-                    permissions: bc?.Permissions ?? Enumerable.Empty<string>(),
-                    userName: bc?.UserName ?? string.Empty,
-                    phoneNumber: bc?.PhoneNumber ?? string.Empty,
-                    displayName: bc?.DisplayName ?? string.Empty,
-                    oauthToken: bc?.OAuthToken ?? string.Empty,
-                    actualTentId: bc?.ActualTenantId ?? string.Empty
-                ));
         }
     }
 }
