3a3fbfdad5d4bc523ac60bed835e628387c6f15d
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-03-16 12:58:44 +0600

    rabbitmq integrated in lmt client

---
+++ src/Blocks.LMT.Client/LmtMessageSenderFactory.cs
@@ -0,0 +1,30 @@
+﻿using SeliseBlocks.LMT.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blocks.LMT.Client
+{
+    public static class LmtMessageSenderFactory
+    {
+        public static ILmtMessageSender Create(LmtOptions options)
+        {
+            if (LmtTransportHelper.IsRabbitMq(options.ConnectionString))
+            {
+                return new LmtRabbitMqSender(
+                    options.ServiceId,
+                    options.ConnectionString,
+                    options.MaxRetries,
+                    options.MaxFailedBatches);
+            }
+
+            return new LmtServiceBusSender(
+                options.ServiceId,
+                options.ConnectionString,
+                options.MaxRetries,
+                options.MaxFailedBatches);
+        }
+    }
+}
