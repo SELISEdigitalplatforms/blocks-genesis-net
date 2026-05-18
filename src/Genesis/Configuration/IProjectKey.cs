46f4e3a525e2beff06d9d39739916741884679d2
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-05-19 03:23:55 +0600

    updated

--- src/Genesis/Configuration/IProjectKey.cs
+++ src/Genesis/Configuration/IProjectKey.cs
@@ -1,5 +1,10 @@
 namespace Blocks.Genesis;
 
-internal interface IProjectKey
+/// <summary>
+/// Deprecated. Do not use.
+/// </summary>
+[System.Obsolete("IProjectKey is deprecated and will be removed in future versions.")]
+public interface IProjectKey
 {
+    string ProjectKey { get; set; }
 }
