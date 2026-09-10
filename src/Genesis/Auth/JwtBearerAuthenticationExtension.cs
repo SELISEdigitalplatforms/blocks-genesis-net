a68049aa39ddddfc5d0bed2904c12a66c2abcc0f
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-09-10 20:13:10 +0600

    StoreBlocksContextInActivity make method public

--- src/Genesis/Auth/JwtBearerAuthenticationExtension.cs
+++ src/Genesis/Auth/JwtBearerAuthenticationExtension.cs
@@ -466,7 +466,7 @@ public static class JwtBearerAuthenticationExtension
         };
     }
 
-    private static void StoreBlocksContextInActivity(BlocksContext context)
+    public static void StoreBlocksContextInActivity(BlocksContext context)
     {
         Baggage.SetBaggage("UserId", context.UserId);
         Baggage.SetBaggage("IsAuthenticate", "true");
